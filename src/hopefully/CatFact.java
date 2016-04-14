8aaf8a169d466f3a91a6f0d9506afc68b3eeb16e
Aaron Zhang <Aaron Zhang>
2016-04-13 23:58:13 -0400

    added basic trivia

--- src/hopefully/CatFact.java
+++ src/hopefully/CatFact.java
@@ -6,39 +6,57 @@ import java.util.ArrayList;
 import org.jibble.pircbot.*;
 public class CatFact extends PircBot {
 	
+	
 	ArrayList<String> cats;
+	ArrayList<String> triviaAnswers;
+	ArrayList<String> triviaQuestions;
 	
 	public CatFact(String name)
 	{
 		setName(name);
 	}
 	
-	public void onMessage(String channel, String sender,String login, String hostname, String message) {
+	
+	public void onMessage(String channel, String sender,String login, String hostname, String message) 
+	{
+		ReadTxtDocuments reader = new ReadTxtDocuments();
 		if (message.equalsIgnoreCase("@catfact"))  
 		{
-			ReadTxtDocuments reader = new ReadTxtDocuments();
 			
-				try {
-					cats = reader.getCatFact();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			System.out.println(cats.size());
-			System.out.println((int)(Math.random()*cats.size()));
-			sendMessage(channel, cats.get((int) (Math.random()*cats.size())));
+			
+			try 
+			{
+				cats = reader.getCatFact();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+
+			sendMessage(channel, cats.get((int)(Math.random()*cats.size())));
 			
 		}
 		if(message.equalsIgnoreCase("@botcommands"))
 		{
 			sendMessage(channel, "@catfact - for a catfact");
-			sendMessage(channel, "@trvia for fun trivia");		
+			sendMessage(channel, "@trivia   - for fun trivia");		
 			
 		}
 		if(message.equalsIgnoreCase("@trvia"))
 		{
-			sendMessage(channel, "@trvia does not work now");
+			try {
+				triviaAnswers = reader.getTriviaAnswers();
+				triviaQuestions = reader.getTriviaQuestions();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			for(int i = 0;i<triviaAnswers.size();i++)
+			{
+
+				System.out.println(triviaAnswers.get(i));
+			}
 		}
 		
 	}
--- src/hopefully/ReadTxtDocuments.java
+++ src/hopefully/ReadTxtDocuments.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class ReadTxtDocuments {
 
 	private ArrayList <String> catfact = new ArrayList<>();
-	
+	private ArrayList <String> triviaQuestions = new ArrayList<>();
+	private ArrayList <String> triviaAnswers = new ArrayList<>();	
 	
 	public ArrayList getCatFact() throws IOException
 	{
@@ -22,6 +23,49 @@ public class ReadTxtDocuments {
 			catfact.add(line);
 		}
 		return catfact;
+	}
+	public ArrayList getTriviaQuestions() throws IOException
+	{
+		BufferedReader re = new BufferedReader(new FileReader("trivia.txt"));
+		
+		int i = 1;
+		String line ="";
+		
+		while((line=re.readLine()) != null)
+		{
+			if(i%2 == 1)
+			{
+				triviaQuestions.add(line);
+			}
+			else
+			{
+				re.readLine();
+			}
+			i++;
+		}
+		return triviaQuestions;
+	}
+	public ArrayList getTriviaAnswers() throws IOException
+	{
+
+		BufferedReader re = new BufferedReader(new FileReader("trivia.txt"));
+		
+		int i = 1;
+		String line ="";
+		
+		while((line=re.readLine()) != null)
+		{
+			if(i%2 == 0)
+			{
+				triviaAnswers.add(line);
+			}
+			else
+			{
+				re.readLine();
+			}
+			i++;
+		}
+		return triviaAnswers;
 	}
 	
 	
